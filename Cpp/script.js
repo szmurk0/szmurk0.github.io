15bc529198da1fd4c5e3166a5a749c4e250d8736
szmurk0 <149005278+szmurk0@users.noreply.github.com>
2023-12-31 15:52:27 +0100

    Update script.js

--- Cpp/script.js
+++ Cpp/script.js
@@ -4,8 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
   fetch('kod.txt')
     .then(response => response.text())
     .then(data => {
-      // Wstaw zawartość pliku do elementu <p> z użyciem innerHTML
-      kodContainer.innerHTML = data;
+      // Zamień znaki nowej linii na znaczniki <br>
+      var formattedData = data.replace(/\n/g, '<br>');
+
+      // Wstaw sformatowaną zawartość pliku do elementu <p> z użyciem innerHTML
+      kodContainer.innerHTML = formattedData;
     })
     .catch(error => {
       console.error('Błąd pobierania pliku:', error);
