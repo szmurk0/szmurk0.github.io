4fe795b244d2e4e021b4224d4dec1d15ad1ba8e6
szmurk0 <149005278+szmurk0@users.noreply.github.com>
2023-12-31 16:07:54 +0100

    Create script2.js

---
+++ Cpp/script2.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var kodContainer = document.getElementById('kod-container');
+
+  fetch('kod2.txt')
+    .then(response => response.text())
+    .then(data => {
+      // Zamień znaki nowej linii na znaczniki <br>
+      var formattedData = data.replace(/\n/g, '<br>');
+
+      // Wstaw sformatowaną zawartość pliku do elementu <p> z użyciem innerHTML
+      kodContainer.innerHTML = formattedData;
+    })
+    .catch(error => {
+      console.error('Błąd pobierania pliku:', error);
+    });
+});
